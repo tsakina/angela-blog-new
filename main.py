2a5b73f80e72d3c4ba0a5e842f29a91d416a1b59
sakinatinwala <sakina903@gmail.com>
2023-02-15 12:42:33 +0530

    Add gunicorn

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("secret_key")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
